40989db6f503b54a04f929484c8f64c865a3c9ac
Lukasz <7lukasz8@gmail.com>
2023-09-25 22:46:36 +0200

    login page initial

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,31 @@
 import styled from "styled-components";
 
 const StyledLoginPage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 100vh;
   width: 100vw;
-  background-color: var(--color-black-200);
+  background-color: var(--color-black-100);
   color: var(--color-white-100);
 `;
 
 const MainBlock = styled.div`
-    background-color: var(--color-black-100);
-    width: 70%;
-    height: 70%;
-`
+  background-color: white;
+  width: 80%;
+  height: 80%;
+`;
+
+// const LoginForm = styled.div{
+//     wi
+// }
 
 function Login() {
-  return <StyledLoginPage>
-    <MainBlock>
-        dsa
-    </MainBlock>
-  </StyledLoginPage>;
+  return (
+    <StyledLoginPage>
+      <MainBlock>dsa</MainBlock>
+    </StyledLoginPage>
+  );
 }
 
 export default Login;
