bfcefec40c31bac1a1000fc881b905dec51a235a
Lukasz <7lukasz8@gmail.com>
2023-11-06 22:02:32 +0100

    deposit window visuals

--- src/features/header/DepositWindow.jsx
+++ src/features/header/DepositWindow.jsx
@@ -1,10 +1,43 @@
 import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
+import Button from "../../ui/Button";
 import { useChangeBalance } from "../../hooks/useChangeBalance";
 import { useState } from "react";
 import { useQueryClient } from "@tanstack/react-query";
+import InputBox from "../../ui/InputBox";
+import { motion } from "framer-motion";
+import { BsCash } from "react-icons/bs";
 
-const StyledDepositWindow = styled.div``;
+const StyledDepositWindow = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  height: 175px;
+  width: 200px;
+`;
+
+const Header = styled.p`
+  font-size: 1.8rem;
+`;
+
+const Input = styled(motion.input)`
+  height: 30px;
+  width: 80px;
+  border: none;
+  flex-grow: 1;
+  padding-left: 8px;
+  font-size: 1.4rem;
+  background-color: transparent;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Info = styled.p`
+  font-size: 1rem;
+`
 
 function DepositWindow({ onCloseModal }) {
   const { changeBalance, isLoading } = useChangeBalance();
@@ -18,21 +51,23 @@ function DepositWindow({ onCloseModal }) {
     changeBalance(newBalance, { onSettled: onCloseModal() });
   }
 
-  //cza to wystylowac
-
   return (
     <StyledDepositWindow>
       {isLoading ? (
         <Spinner />
       ) : (
         <>
-          <p>Declare deposit amount</p>
-          <input
-            type="number"
-            value={deposit}
-            onChange={(e) => setDeposit(e.target.value)}
-          />
-          <button onClick={onClickAction}>deposit</button>
+          <Header>Declare deposit amount</Header>
+          <InputBox icon={<BsCash />}>
+            <Input
+              placeholder="Deposit"
+              type="number"
+              value={deposit}
+              onChange={(e) => setDeposit(e.target.value)}
+            />
+          </InputBox>
+          <Button onClick={onClickAction}>Deposit</Button>
+          <Info>In the future, i will implement here some PayU or something</Info>
         </>
       )}
     </StyledDepositWindow>
