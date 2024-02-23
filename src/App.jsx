81889f68f54ad48de859186673975ab6ee15d76b
Lukasz <7lukasz8@gmail.com>
2024-02-23 20:50:54 +0100

    react toasts config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster, useToasterStore } from "react-hot-toast";
 import AppLayout from "./ui/AppLayout";
 import Dashboard from "./pages/Dashboard";
 import Account from "./pages/Account";
@@ -12,6 +12,7 @@ import ProtectedRoute from "./features/autentication/ProtectedRoute";
 import Stock from "./pages/Stock";
 import Stats from "./pages/Stats";
 import Currency from "./features/stock/Currency";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,6 +24,14 @@ const queryClient = new QueryClient({
 });
 
 function App() {
+  const { toasts } = useToasterStore();
+  const TOAST_LIMIT = 3;
+  useEffect(() => {
+    toasts
+      .filter((t) => t.visible) 
+      .filter((_, i) => i >= TOAST_LIMIT)
+      .forEach((t) => toast.dismiss(t.id));
+  }, [toasts]);
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
